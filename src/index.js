4b1aeaad873615db0f256e9ecdee6495107886ef
Artur Charaev <reaverart@gmail.com>
2019-07-01 15:06:03 +0300

    Add Sequenced Requests (#3)
    * first pass on queue
    
    * queue requests tweaks
    
    * add skip request to queue
    
    * add skip action
    
    * up test
    
    * Revert "up test"
    
    This reverts commit 407535e5e5e196d4112b5b8dd19486eeea1de5c3.
    
    * update test
    
    * 0.6.0-beta
    
    * fix typo
    
    * 0.6.0-beta1
    
    * fix dispatcher and update tests
    
    * 0.6.0-beta2
    
    * normalize api
    
    * readme and package json up
    
    * 0.6.0-beta3
    
    * action as a function might return string action type, wrap it to FSA
    
    * 0.6.0-beta4
    
    * tweaks to skip requests, clean up
    
    * 0.6.0-beta5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export const CALL_API = Symbol('Call API');
 export const CALL_API_PHASE = Symbol('Call API phase');
+export const CALL_API_SKIP_REQUEST = Symbol('Call API skip request');
+export const CALL_API_SKIP_ACTION = Symbol('Call API skip action');
 
 const REQUEST = 'REQUEST';
 const SUCCESS = 'SUCCESS';
@@ -11,17 +13,21 @@ export const callApiPhases = {
   FAILURE,
 };
 
+const actionTypeToFSA = (actionType) => {
+  if (typeof actionType === 'string' || typeof actionType === 'symbol') {
+    return { type: actionType };
+  }
+  return actionType;
+};
+
 export const actionWith = (actionType, args, payload) => {
   let nextAction;
   if (typeof actionType === 'function') {
     nextAction = actionType(...args, payload);
+    // it might return CALL_API_SKIP_ACTION so need to wrap it into FSA
+    nextAction = actionTypeToFSA(nextAction);
   } else {
-    // convert strings or symbols to FSA object
-    if (typeof actionType === 'string' || typeof actionType === 'symbol') {
-      nextAction = { type: actionType };
-    } else {
-      nextAction = actionType;
-    }
+    nextAction = actionTypeToFSA(actionType);
 
     if (payload) {
       nextAction.payload = payload;
@@ -37,7 +43,7 @@ export const actionWith = (actionType, args, payload) => {
     phase = REQUEST;
   } else if (payload instanceof Error) {
     phase = FAILURE;
-  } else if (payload !== undefined) {
+  } else {
     phase = SUCCESS;
   }
   nextAction[CALL_API_PHASE] = phase;
@@ -45,12 +51,77 @@ export const actionWith = (actionType, args, payload) => {
   return nextAction;
 };
 
-const normalize = (item, apiAction, getState) => {
+const normalizeSkip = (skip, apiAction, getState) => {
+  switch (typeof skip) {
+    case 'boolean':
+      return CALL_API_SKIP_REQUEST;
+    case 'function':
+      return skip(apiAction, getState);
+    default:
+      return skip;
+  }
+};
+
+const normalizeParams = (item, apiAction, getState) => {
   if (typeof item === 'function') {
     return item(apiAction, getState());
   }
   return item;
 };
+
+const prepareRequests = (data) => {
+  let { batch } = data;
+  const batchMode = Array.isArray(batch);
+  if (!batchMode) {
+    const { endpoint, options, [CALL_API_SKIP_REQUEST]: skipRequest } = data;
+    batch = [{ endpoint, options, [CALL_API_SKIP_REQUEST]: skipRequest }];
+  }
+  return { batch, batchMode };
+};
+
+const normalizeRequests = (requestsOptions, args) => (
+  requestsOptions.map((request) => {
+    if (request[CALL_API_SKIP_REQUEST]) {
+      return {
+        [CALL_API_SKIP_REQUEST]: normalizeSkip(request[CALL_API_SKIP_REQUEST], ...args),
+      };
+    }
+    return {
+      endpoint: normalizeParams(request.endpoint, ...args),
+      options: normalizeParams(request.options, ...args),
+    };
+  })
+);
+
+const performRequests = (requests, callApi) => (
+  requests.map(({ endpoint, options, [CALL_API_SKIP_REQUEST]: resolveWith }) => (
+    resolveWith === undefined
+      ? callApi(endpoint, options)
+      : Promise.resolve(resolveWith)
+  ))
+);
+
+const dispatcher = (dispatch, action) => {
+  if (action.type === CALL_API_SKIP_ACTION) {
+    return false;
+  }
+  return dispatch(action);
+};
+
+const makeRequestsQueue = (callApi, queue) => (
+  (apiAction, getState, responses) => (
+    queue.reduce((memo, item, i) => (
+      memo.then((res) => {
+        const requestsData = item(apiAction, getState(), res);
+        const { batch } = prepareRequests(requestsData);
+        let requests = normalizeRequests(batch, [apiAction, getState]);
+        requests = performRequests(requests, callApi);
+
+        return Promise.all(requests).then(queueRes => res.concat(queueRes));
+      })
+    ), Promise.resolve(responses))
+  )
+);
 
 export const createMiddleware = ({
   callApi,
@@ -59,43 +130,37 @@ export const createMiddleware = ({
     if (!action[CALL_API]) {
       return next(action);
     }
-
     const apiAction = action[CALL_API];
-    let { batch } = apiAction;
-    const { endpoint, options } = apiAction;
-    const batchMode = Array.isArray(batch);
-
-    if (!batchMode) {
-      batch = [{ endpoint, options }];
-    }
-    // prepare requests params
-    batch = batch.map(request => ({
-      endpoint: normalize(request.endpoint, apiAction, getState),
-      options: normalize(request.options, apiAction, getState),
-    }));
 
     // action types
     const types = apiAction.type
       ? Array(3).fill(apiAction.type)
       : apiAction.types;
-
     const [requestType, successType, failureType] = types;
 
+    const { queue } = apiAction;
+    const queueMode = Array.isArray(queue);
+    const { batch, batchMode } = prepareRequests(apiAction);
+    const requests = normalizeRequests(batch, [apiAction, getState]);
+
     // dispatch request type
-    dispatch(actionWith(
+    dispatcher(dispatch, actionWith(
       requestType, [apiAction, getState()]
     ));
 
-    const promises = batch.map(request =>
-      callApi(request.endpoint, request.options)
-    );
+    const promises = performRequests(requests, callApi);
 
     return Promise.all(promises)
+      .then(responses => (
+        queueMode
+          ? makeRequestsQueue(callApi, queue)(apiAction, getState, responses)
+          : responses
+      ))
       .then(
-        responses => dispatch(actionWith(
-          successType, [apiAction, getState()], batchMode ? responses : responses[0]
+        responses => dispatcher(dispatch, actionWith(
+          successType, [apiAction, getState()], (batchMode || queueMode) ? responses : responses[0]
         )),
-        error => dispatch(actionWith(
+        error => dispatcher(dispatch, actionWith(
           failureType, [apiAction, getState()], error
         ))
       );
--- test/index.js
+++ test/index.js
@@ -3,17 +3,22 @@ import sinon from 'sinon';
 import createApiMiddleware, {
   CALL_API,
   CALL_API_PHASE,
+  CALL_API_SKIP_REQUEST,
+  CALL_API_SKIP_ACTION,
   callApiPhases,
 } from '../src/index';
 
-function makeSleep(ms, shouldResolve, response) {
-  return () =>
-    new Promise((resolve, reject) => (
+function makeSleep(ms, shouldResolve, response, spy) {
+  return (...args) => {
+    if (spy) {
+      spy(...args);
+    }
+    return new Promise((resolve, reject) => (
       setTimeout(() => (
         shouldResolve ? resolve(response) : reject(response)
       ), ms)
-    )
-  );
+    ));
+  };
 }
 
 test('Calls fetch function with given endpoint and options', (t) => {
@@ -91,7 +96,7 @@ test('Calls batch requests', (t) => {
   t.end();
 });
 
-test('Dispatches request action', (t) => {
+test('Dispatches REQUEST action', (t) => {
   const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, true, {}) });
   const doGetState = () => {};
   const doDispatch = sinon.spy();
@@ -113,7 +118,7 @@ test('Dispatches request action', (t) => {
   });
 });
 
-test('Dispatches success action', (t) => {
+test('Dispatches SUCCESS action', (t) => {
   const RESPONSE = 'RESPONSE';
   const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, true, RESPONSE) });
   const doGetState = () => {};
@@ -137,7 +142,7 @@ test('Dispatches success action', (t) => {
   });
 });
 
-test('Dispatches failure action', (t) => {
+test('Dispatches FAILURE action', (t) => {
   const RESPONSE = new Error();
   const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, false, RESPONSE) });
   const doGetState = () => {};
@@ -180,13 +185,13 @@ test('Dispatches single typed action with request and success action phase', (t)
   }).then(() => {
     t.true(doDispatch.firstCall.calledWith({
       type: ACTION_TYPE,
-      [CALL_API_PHASE]: callApiPhases.REQUEST,
     }));
+    t.true(doDispatch.firstCall.args[0][CALL_API_PHASE] === callApiPhases.REQUEST);
     t.true(doDispatch.secondCall.calledWith({
       type: ACTION_TYPE,
-      [CALL_API_PHASE]: callApiPhases.SUCCESS,
       payload: RESPONSE,
     }));
+    t.true(doDispatch.secondCall.args[0][CALL_API_PHASE] === callApiPhases.SUCCESS);
     t.true(doDispatch.calledTwice);
     t.end();
   });
@@ -210,15 +215,154 @@ test('Dispatches single typed action with request and failure action phase', (t)
   }).then(() => {
     t.true(doDispatch.firstCall.calledWith({
       type: ACTION_TYPE,
-      [CALL_API_PHASE]: callApiPhases.REQUEST,
     }));
+    t.true(doDispatch.firstCall.args[0][CALL_API_PHASE] === callApiPhases.REQUEST);
     t.true(doDispatch.secondCall.calledWith({
       type: ACTION_TYPE,
-      [CALL_API_PHASE]: callApiPhases.FALIURE,
       payload: RESPONSE,
       error: true,
     }));
+    t.true(doDispatch.secondCall.args[0][CALL_API_PHASE] === callApiPhases.FAILURE);
     t.true(doDispatch.calledTwice);
+    t.end();
+  });
+});
+
+test('Calls batch and queue requests', (t) => {
+  const spy = sinon.spy();
+  const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, true, 'RESPONSE', spy) });
+  const doGetState = () => {};
+  const doDispatch = action => action;
+  const nextHandler = apiMiddleware({ getState: doGetState, dispatch: doDispatch });
+  const doNext = () => {};
+  const actionHandler = nextHandler(doNext);
+  const ENDPOINT = 'ENDPOINT';
+  const OPTIONS = {};
+
+  actionHandler({
+    [CALL_API]: {
+      types: ['REQUEST', 'SUCCESS', 'FAILURE'],
+      batch: [
+        {
+          endpoint: () => ENDPOINT,
+          options: () => OPTIONS,
+        },
+        {
+          endpoint: () => ENDPOINT,
+          options: () => OPTIONS,
+        },
+      ],
+      queue: [
+        () => ({
+          batch: [
+            {
+              endpoint: () => ENDPOINT,
+              options: () => OPTIONS,
+            },
+            {
+              [CALL_API_SKIP_REQUEST]: () => [{ skipData: 'SKIP' }],
+            },
+          ],
+        }),
+        () => ({
+          [CALL_API_SKIP_REQUEST]: () => [{ skipData: 'SKIP' }],
+        }),
+        () => ({
+          [CALL_API_SKIP_REQUEST]: [CALL_API_SKIP_REQUEST],
+        }),
+        () => ({
+          endpoint: 'test',
+          options: OPTIONS,
+        }),
+      ],
+    },
+  }).then((action) => {
+    t.true(spy.callCount === 4);
+    t.deepEqual(action.payload, ['RESPONSE', 'RESPONSE', 'RESPONSE', [{ skipData: 'SKIP' }], [{ skipData: 'SKIP' }], [CALL_API_SKIP_REQUEST], 'RESPONSE']);
+    t.end();
+  });
+});
+
+test('Skips REQUEST action', (t) => {
+  const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, true, 'RESPONSE') });
+  const doGetState = () => {};
+  const spy = sinon.spy();
+  const doDispatch = (action) => { spy(action); return action; };
+  const nextHandler = apiMiddleware({ getState: doGetState, dispatch: doDispatch });
+  const doNext = () => {};
+  const actionHandler = nextHandler(doNext);
+  const ENDPOINT = 'ENDPOINT';
+  const OPTIONS = {};
+
+  actionHandler({
+    [CALL_API]: {
+      types: [CALL_API_SKIP_ACTION, 'SUCCESS', 'FAILURE'],
+      endpoint: ENDPOINT,
+      options: OPTIONS,
+    },
+  }).then((action) => {
+    t.true(spy.calledOnce);
+    t.true(spy.calledWith({
+      type: 'SUCCESS',
+      payload: 'RESPONSE',
+    }));
+    t.deepEqual(action, {
+      type: 'SUCCESS',
+      payload: 'RESPONSE',
+    });
+    t.end();
+  });
+});
+
+test('Skips SUCCESS action', (t) => {
+  const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, true, 'RESPONSE') });
+  const doGetState = () => {};
+  const doDispatch = sinon.spy();
+  const nextHandler = apiMiddleware({ getState: doGetState, dispatch: doDispatch });
+  const doNext = () => {};
+  const actionHandler = nextHandler(doNext);
+  const ENDPOINT = 'ENDPOINT';
+  const OPTIONS = {};
+
+  actionHandler({
+    [CALL_API]: {
+      types: ['REQUEST', CALL_API_SKIP_ACTION, 'FAILURE'],
+      endpoint: ENDPOINT,
+      options: OPTIONS,
+    },
+  }).then((action) => {
+    t.true(action === false);
+    t.true(doDispatch.calledOnce);
+    t.true(doDispatch.calledWith({
+      type: 'REQUEST',
+    }));
+    t.end();
+  });
+});
+
+test('Skips FAILURE action', (t) => {
+  const error = new Error('test');
+  const apiMiddleware = createApiMiddleware({ callApi: makeSleep(0, false, error) });
+  const doGetState = () => {};
+  const doDispatch = sinon.spy();
+  const nextHandler = apiMiddleware({ getState: doGetState, dispatch: doDispatch });
+  const doNext = () => {};
+  const actionHandler = nextHandler(doNext);
+  const ENDPOINT = 'ENDPOINT';
+  const OPTIONS = {};
+
+  actionHandler({
+    [CALL_API]: {
+      types: ['REQUEST', 'SUCCESS', CALL_API_SKIP_ACTION],
+      endpoint: ENDPOINT,
+      options: OPTIONS,
+    },
+  }).then((action) => {
+    t.true(action === false);
+    t.true(doDispatch.calledOnce);
+    t.true(doDispatch.calledWith({
+      type: 'REQUEST',
+    }));
     t.end();
   });
 });
